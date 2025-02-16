5ec743cffefdcc873723b3aea1835e1290055acf
Dean White <de4nwhit3@gmail.com>
2025-02-16 17:42:28 +0200

    added equatable

--- lib/application/pages/advice/bloc/bloc/advisor_event.dart
+++ lib/application/pages/advice/bloc/bloc/advisor_event.dart
@@ -1,10 +1,13 @@
 part of 'advisor_bloc.dart';
 
 @immutable
-sealed class AdvisorEvent {}
+sealed class AdvisorEvent extends Equatable {}
 
 class RequestAdviceEvent extends AdvisorEvent {
   final String param;
 
   RequestAdviceEvent({required this.param});
+
+  @override
+  List<Object?> get props => [];
 }
--- lib/application/pages/advice/bloc/bloc/advisor_state.dart
+++ lib/application/pages/advice/bloc/bloc/advisor_state.dart
@@ -1,7 +1,10 @@
 part of 'advisor_bloc.dart';
 
 @immutable
-sealed class AdvisorState {}
+sealed class AdvisorState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class AdvisorInitialState extends AdvisorState {}
 
@@ -9,6 +12,9 @@ final class AdvisorErrorState extends AdvisorState {
   final String errorMessage;
 
   AdvisorErrorState({required this.errorMessage});
+
+  @override
+  List<Object?> get props => [errorMessage];
 }
 
 final class AdvisorLoadingState extends AdvisorState {}
@@ -17,4 +23,7 @@ final class AdvisorLoadedState extends AdvisorState {
   final String advice;
 
   AdvisorLoadedState({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
 }
