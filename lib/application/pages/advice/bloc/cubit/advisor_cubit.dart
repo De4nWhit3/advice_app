dbe57cd5938499440c2b276e781a75cdd74359c1
Dean White <de4nwhit3@gmail.com>
2025-02-17 19:13:33 +0200

    added failures and updated messages

--- lib/application/pages/advice/bloc/cubit/advisor_cubit.dart
+++ lib/application/pages/advice/bloc/cubit/advisor_cubit.dart
@@ -1,8 +1,13 @@
+import 'package:advisor/domain/failures/failures.dart';
 import 'package:advisor/domain/use_cases/advice_usecase.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'advisor_state.dart';
+
+String serverFailureMessage = 'Server failure occurred!';
+String cacheFailureMessage = 'Cache failure occurred!';
+String defaultFailureMessage = 'Default error OR general failure occurred!';
 
 class AdvisorCubit extends Cubit<AdvisorCubitState> {
   AdvisorCubit() : super(AdvisorCubitStateInitial());
@@ -11,8 +16,25 @@ class AdvisorCubit extends Cubit<AdvisorCubitState> {
 
   void adviceRequestedEvent() async {
     emit(AdvisorCubitStateLoading());
-    final advice = await adviceUsecase.getAdvice();
+    final result = await adviceUsecase.getAdvice();
     // emit(AdvisorCubitStateError(errorMessage: 'Error message from cubit'));
-    emit(AdvisorCubitStateLoaded(advice: advice.advice));
+    result.fold(
+      ifLeft:
+          (value) => emit(
+            AdvisorCubitStateError(errorMessage: _mapFailureToMessage(value)),
+          ),
+      ifRight: (value) => emit(AdvisorCubitStateLoaded(advice: value.advice)),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return serverFailureMessage;
+      case CacheFailure:
+        return cacheFailureMessage;
+      default:
+        return defaultFailureMessage;
+    }
   }
 }
--- lib/domain/use_cases/advice_usecase.dart
+++ lib/domain/use_cases/advice_usecase.dart
@@ -1,8 +1,13 @@
 import 'package:advisor/domain/entities/advice_entity.dart';
+import 'package:advisor/domain/failures/failures.dart';
+import 'package:dart_either/dart_either.dart';
 
 class AdviceUsecase {
-  Future<AdviceEntity> getAdvice() async {
-    await Future.delayed(Duration(seconds: 1));
-    return AdviceEntity(id: 1, advice: 'Fake advice entity from use case');
+  Future<Either<Failure, AdviceEntity>> getAdvice() async {
+    await Future.delayed(Duration(milliseconds: 200));
+    // return Right(
+    //   AdviceEntity(id: 1, advice: 'Fake advice entity from use case'),
+    // );
+    return Left(GeneralFailure());
   }
 }
